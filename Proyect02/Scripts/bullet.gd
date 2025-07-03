3df5793a71c77c9131045ea8357a28e14cd7c29b
Joaquin Franich <joaquinmelo90@gmail.com>
2025-07-03 20:58:44 -0300

    Bullet-time fixed (still need improvement).
    Se corrigio el funcionamiento the bullet time. Ya no se aplica sobre el glibal_time. Se aplica sobre el local_time. Desafectando la UI y los Menus.
    
    Queda corregir la funcion Bounce() de el nodo Enemy: no se aplica el bullet time ya que esta funcion no utiliza "delta time".

--- Proyect02/Scripts/bullet.gd
+++ Proyect02/Scripts/bullet.gd
@@ -4,8 +4,17 @@ extends Area2D
 @export var speed = 600
 @export var damage = 1
 
+var time_scale := 1.0
+
+func set_time_scale(scale: float):
+	time_scale = scale
+
+func _ready():
+	add_to_group("gameplay")
+
 func _physics_process(delta):
-	global_position.y += -speed * delta
+	var scaled_delta = delta * time_scale
+	global_position.y += -speed * scaled_delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
